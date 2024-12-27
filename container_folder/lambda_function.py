3a9720aca56de1fd582870499e3c2437786375b1
johnardavies <johnardavies@gmail.com>
2024-12-27 17:28:54 +0000

    black formatter

--- container_folder/lambda_function.py
+++ container_folder/lambda_function.py
@@ -3,13 +3,20 @@ import numpy as np
 import pandas as pd
 import json
 
-def handler(event, context):
 
+def handler(event, context):
+    """Function that takes input string and imports numpy and pandas returning their versions as a json"""
     query_params = event.get("queryStringParameters", {})
     input = query_params.get("input", "world")
 
     # Return the result as JSON
     return {
         "statusCode": 200,
-        "body": json.dumps({"hello": str(input), "numpy version": str(np.__version__), "pandas version": str(pd.__version__)})
+        "body": json.dumps(
+            {
+                "hello": str(input),
+                "numpy version": str(np.__version__),
+                "pandas version": str(pd.__version__),
+            }
+        ),
     }
